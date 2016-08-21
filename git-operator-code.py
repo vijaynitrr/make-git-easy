4c896164aa610c2293d2577186a58eaacdf6e05e
vijaynitrr <vijaykhandalnitrr@gmail.com>
2016-08-21 21:17:04 +0530

    login functionality at basic level

--- git-operator-code.py
+++ git-operator-code.py
@@ -95,8 +95,7 @@ class EasyGitApp(QtWidgets.QMainWindow, easyGit.Ui_MainWindow):
             self.textOutput.append('> '+self.command+"\r\n"+self.output[1])
             self.output = commands.getstatusoutput('grep -i "machine github.com" ~/.netrc | wc -l')
             lastIndex = len(self.output[1]) -1 
-            print self.output[1][lastIndex]
-            if self.output[1] == '0':
+            if self.output[1][lastIndex] == '0':
                 self.userLogin()
                 self.pushLine = 'machine github.com login ' + self.username + ' password '+self.password
                 self.output = commands.getstatusoutput('echo ' + self.pushLine + ' | cat > ~/.netrc')
